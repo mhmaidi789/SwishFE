3f032dd87688f401834128e3e16291dc83c85c83
moAptos <mhmaidi789@gmail.com>
2023-08-14 13:13:03 -1000

    inited with ts

---
+++ swish_fe/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Swish FE</p>
+      </header>
+      <body className="App-body">
+        <p>Helloooo</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
